3e01894178940fd17d4acad7e71c8d1d2056af46
Abdolrahman Mahmoud <283abdoabdo@gmail.com>
2025-02-17 19:07:09 +0200

    handel favIcon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Abdurrahman M. Alazhari",
   description: "Frontend Developer Specializing in React.js and Next.js",
+  icons: {
+    icon: "../public/favicon.ico",
+    apple: "../public/favicon-16x16.png",
+    shortcut: "../public/favicon-32x32.png",
+  },
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />{" "}
       </head>
       <body className={inter.className}>
         <ThemeProvider
